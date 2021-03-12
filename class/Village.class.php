0df1f38dac2606013c6cfd4c646a0c6d5c738aa2
Dziwne-u-mie-Dziala <dziwne_u_mnie_dziala@wp.pl>
2021-03-12 11:54:01 +0100

    Produkcja drewna

--- class/Village.class.php
+++ class/Village.class.php
@@ -3,6 +3,7 @@
 class Village 
 {
     private $buildings;
+    private $storage;
 
     public function __construct()
     {
@@ -10,6 +11,22 @@ class Village
             'townHall' => 1,
             'woodcutter' => 1,
         );
+        $this->storage = array(
+            'wood' => 0,
+        );
+    }
+    private function woodGain(int $deltaTime) : float
+    {
+        //liczymy zysk na godzine z wzoru poziom_drwala ^ 2
+        $gain = pow($this->buildings['woodcutter'],2) * 100;
+        // liczymy zysk na sekunde (godzina/3600)
+        $perSecondGain = $gain / 3600;
+        //zwracamy zysk w czasie $deltaTime
+        return $perSecondGain * $deltaTime;
+    }
+    public function gain($deltaTime) 
+    {
+        $this->storage['wood'] += $this->woodGain($deltaTime);
     }
 }
 
--- index.php
+++ index.php
@@ -7,28 +7,34 @@
     <title>Osadnicy - gra przeglądarkowa</title>
 </head>
 <body>
-    <?php echo "dziala"; ?>
-    <pre>
-    <?php 
-        require('./class/Village.class.php');
-        session_start();
+<?php 
+    require('./class/Village.class.php');
+    session_start();
 
 
 
-        if(!isset($_SESSION['v'])) // jeżeli nie ma w sesji naszej wioski
-        {
-            echo "Tworzę nową wioskę...";
-            $v = new Village();
-            $_SESSION['v'] = $v;
-        } 
-        else //mamy już wioskę w sesji - przywróć ją
-        {
-            $v = $_SESSION['v'];
-        }
 
 
-        var_dump($v);
-    ?>
-    </pre>
+    if(!isset($_SESSION['v'])) // jeżeli nie ma w sesji naszej wioski
+    {
+        echo "Tworzę nową wioskę...";
+        $v = new Village();
+        $_SESSION['v'] = $v;
+        //reset czasu od ostatniego odświerzenia strony
+        $deltaTime = 0;
+    } 
+    else //mamy już wioskę w sesji - przywróć ją
+    {
+        $v = $_SESSION['v'];
+        //ilosc sekund od ostatniego odświerzenia strony
+        $deltaTime = time() - $_SESSION['time'];
+    }
+
+    $v->gain($deltaTime);
+
+    $_SESSION['time'] = time();
+
+    var_dump($v);
+?>
 </body>
 </html>
